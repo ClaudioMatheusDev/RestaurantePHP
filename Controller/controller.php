cc71506be97ee0099573a7fd6f80f142d847238d
Claudio Matheus <claudiomatheus055@gmail.com>
2024-09-06 13:42:41 -0300

    Adicionando a tela incial.

--- Controller/controller.php
+++ Controller/controller.php
@@ -1,5 +1,28 @@
 <?php
 
+require "Model/model.php";
+require "View/TelaLogin/view.php";
+
+
+class Controller
+{
+
+    private $view;
+    private $conn;
+
+    public $model;
+
+
+    public function Monta_Home()
+    {
+
+        $view = new View();
+        $view->MontaHome();
+    }
+
+
+
+}
 
 
 
---
+++ View/TelaLogin/view.php
@@ -0,0 +1,44 @@
+<?php
+
+class View
+{
+
+
+    public function MontaHome()
+    {
+        echo '<div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Cardapio</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Ver historico de pedidos</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Faça um novo pedido!</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>';
+
+    }//Função montando o Menu Inicial
+
+
+
+}
+
+?>
